01cd387f16440e1c6a7eb05883d5fde4e5131e90
Julio Caballero <88751689+jucecal@users.noreply.github.com>
2022-08-02 17:58:45 -0600

    ya

--- src/modelos/modeloventa_credito.js
+++ src/modelos/modeloventa_credito.js
@@ -3,19 +3,19 @@ const db = require('../configuracion/db');
 const VentasCredito = db.define(
     'ventacredito',
     {
-        IdCredito:{
+        IdCredito: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true,
             allowNull: false,
             field: 'IdCredito' //Nombre del campo en la base de datos
         },
-        IdVenta:{
+        IdVenta: {
             type: DataTypes.STRING(20),
             allowNull: false,
             field: 'IdVenta' //Nombre del campo en la base de datos
         },
-        Activo:{
+        Activo: {
             type: DataTypes.TINYINT(1),
             allowNull: false,
             default: 1,
